ff5cf0ab414a24873a7680da134dc83cbc5cd112
Nikita Semakov <SemakovNV@ekassir.com>
2016-12-09 13:03:42 +0300

    Throw was added to AbstractSerializableModelConverter

--- modules/networking-api/NetworkingApi/Sources/Network/Converter/AbstractSerializableModelConverter.swift
+++ modules/networking-api/NetworkingApi/Sources/Network/Converter/AbstractSerializableModelConverter.swift
@@ -40,6 +40,9 @@ public class AbstractSerializableModelConverter<T: ParcelableModel>: AbstractCal
                 if let jsonObject = (json.object as? JsonObject) {
                     newBody = try T.init(params: jsonObject)
                 }
+                else {
+                    throw ConversionError(entity: entity)
+                }
             }
             catch let exception as JsonSyntaxException {
                 throw ConversionError(entity: entity, cause: exception)
