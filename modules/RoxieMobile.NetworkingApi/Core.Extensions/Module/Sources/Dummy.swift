709c7d7f3a2a950c4ac2aa151de2a3485c3172d0
Alexander Bragin <alexander.bragin@gmail.com>
2017-10-18 01:25:10 +0300

    Added module ‘NetworkingApi.Core.Extensions’.

---
+++ modules/RoxieMobile.NetworkingApi/Core.Extensions/Module/Sources/Dummy.swift
@@ -0,0 +1,16 @@
+// ----------------------------------------------------------------------------
+//
+//  Dummy.swift
+//
+//  @author     Alexander Bragin <bragin-av@roxiemobile.com>
+//  @copyright  Copyright (c) 2017, Roxie Mobile Ltd. All rights reserved.
+//  @link       http://www.roxiemobile.com/
+//
+// ----------------------------------------------------------------------------
+
+public struct Dummy {
+    // Do nothing
+}
+
+// ----------------------------------------------------------------------------
+
