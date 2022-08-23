52dba73cc51636f71039e0aab2e7eba3c80dd2f8
Alexander Bragin <bragin-av@roxiemobile.com>
2022-08-23 13:11:39 +0300

    NSFPIO-1110 Removed obsolete and unused code.

--- pod_trunk_push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pod trunk push --synchronous NetworkingApiObjC.podspec
-pod trunk push --synchronous NetworkingApiHttp.podspec
-pod trunk push --synchronous NetworkingApiHelpers.podspec
-pod trunk push --synchronous NetworkingApiRest.podspec
-pod trunk push --synchronous NetworkingApiConverters.podspec
-pod trunk push --synchronous NetworkingApi.podspec
